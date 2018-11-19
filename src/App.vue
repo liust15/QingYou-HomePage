ffdfa630b432e94abc20f93c9952092438672c7b
UZIhuhuhu <1908341144@qq.com>
2018-11-19 09:20:41 +0800

    feat: finish qinyou-blog3.0 project

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <Home/>
+    <Stuff />
+    <Project />
+    <Recruit />
+    <OpenSource />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Home from "./components/home";
+import Stuff from "./components/stuff";
+import Project from "./components/project";
+import Recruit from "./components/recruit-new";
+import OpenSource from "./components/open-source";
+import Footer from "./components/footer";
+export default {
+  name: "app",
+  components: {
+    Home,
+    Stuff,
+    Project,
+    Recruit,
+    OpenSource,
+    Footer
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+*
+  padding: 0
+  margin: 0
+</style>
